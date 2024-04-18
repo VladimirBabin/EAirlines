7cd6a646ef89e3ff659eb41c6cc4c91e1882ff9d
VladimirBabin <vladimir.sol.rojo@gmail.com>
2024-04-18 20:31:00 +0300

    Added EpamAirlinesApp class

---
+++ src/main/java/com/epamtraining/javastack/epamairlines/EpamAirlinesApp.java
@@ -0,0 +1,7 @@
+package com.epamtraining.javastack.epamairlines;
+
+public class EpamAirlinesApp {
+    public static void main(String[] args) {
+        System.out.println("Hello from Epam airlines!");
+    }
+}
